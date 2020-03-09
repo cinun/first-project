6440803a0087bbe6afcb6188816cc1c093180265
cinun <43857696+cinun@users.noreply.github.com>
2020-03-09 13:03:24 -0500

    Update There.java

--- There.java
+++ There.java
@@ -10,7 +10,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 import wazza.UnoProj.Node;
-
+import java.io.*;
 public class CreateDeck {
 
 	class Node{
@@ -27,12 +27,23 @@ public class CreateDeck {
 			this.prev = null; 
 			}
 		}
+	class Sent{
+		String sentence; 
+		Sent next; 
+		public Sent(String sentence) {
+			this.sentence = sentence; 
+		}
+			
+	}
 	
 	public Node head = null; 
 	public Node tail = null; 
+	public Sent text_head = null; 
+	public Sent text_tail = null; 
 	
 	public static CreateDeck list = new CreateDeck(); 
 	public static CreateDeck hand = new CreateDeck();
+	public static CreateDeck pen = new CreateDeck(); 
 	
 	public void addNode(int data, int index, String color) {
 		
@@ -49,11 +60,34 @@ public class CreateDeck {
 		}
 	}
 	
+	public void display_xyz() {
+		Sent current = text_head; 
+		while(current!=null) {
+			System.out.println(current.sentence);
+			//output.write(current.sentence);
+			current = current.next;
+		}
+	}
+	
+	public void create_sentence(String sentence) {
+		Sent newText = new Sent(sentence); 
+		
+		if (text_head == null) {
+			text_head = newText; 
+			text_tail = newText; 
+		}
+		else {
+			text_tail.next = newText; 
+			text_tail = newText;
+		}
+	}
+	
 	public void display() {
 		Node current = head ; 
 		String above[] = {"+4", "Wild", "Reverse", "+2", "Skip"};
 		if (current == null) {
 			System.out.println("List is empty.\n");
+			
 			return; 
 		}
 		//System.out.println("Nodes of singly linked list:\n");
@@ -66,6 +100,8 @@ public class CreateDeck {
 		}
 		System.out.println(); 
 	}
+	
+	
 	
 	public void shuffle(int deck_count) {
 		Random r = new Random();
@@ -160,13 +196,19 @@ public class CreateDeck {
         } 
         return -1; 
     } 
-
+	/* Project 2 UNO Game 
+	 * class Hand
+	 *By: Sushil G C
+	 *Professor: Mario Pitalua Rodriguez
+	 *Object Oriented Programming CS 2365-003 Spring 2020
+	 *This class is used to create hand and distribute seven cards to individual player in the UNO Game
+	 */
 	public void sortBubble(String check_color[]) {
 		Node current; 
-		for (int i = 0 ;i <7; i++) {
+		for (int i = 0 ;i <7; i++) { // loops until an individual gets seven cards
 			current = head; 
 			for (int j = 0; j < 6; j++){
-				if (findIndex(check_color, current.color) < findIndex(check_color, current.next.color)) {
+				if (findIndex(check_color, current.color) < findIndex(check_color, current.next.color)) { //checks the color of the card distributed
 					int temp_data = current.data;
 					String temp_color = current.color; 
 					int temp_index = i; 	//current.index; 
@@ -183,7 +225,7 @@ public class CreateDeck {
 	}
 	public void sortRank() {
 		Node current; 
-		for (int i = 0 ;i <7; i++) {
+		for (int i = 0 ;i <7; i++) {  // cards are ranked in ascending order
 			current = head; 
 			for (int j = 0; j < 6; j++){
 				if ((current.data > current.next.data) && (current.color == current.next.color)) {
@@ -197,7 +239,7 @@ public class CreateDeck {
 	}
 	public void pop(String del) {
 		Node current = head; 
-		//Delete one elem
+		//Delete one element
 	}
 	public void delete_hand() {
 		//Method that removes 7 card from the deck to form a hand
@@ -211,6 +253,51 @@ public class CreateDeck {
 		list.tail = newNode;
 		
 	}
+	/*
+	public void display_text() throws FileNotFoundException {
+		Sent current = text_head; 
+		PrintWriter output =  new PrintWriter(new File("output.txt"));
+		output.write("<html>"); 
+		output.println("<head>"); 
+		output.println("<title>"); 
+		output.println("Uno Game in Html </title>"); 
+		output.println("</head>"); 
+		output.println("<body>"); 
+		while(current!=null) {
+			String table = "<p>"+current.sentence+"</p>";
+			System.out.print("k cha bro?\n"); 
+			output.println(table); 
+			current = current.next; 
+		}
+		output.println("</body>"); 
+		output.println("</html>"); 
+	}*/
+	
+	public void display_text() throws FileNotFoundException{
+		try {
+			File file = new File("Dataout.html"); 
+			Writer output = new BufferedWriter(new FileWriter(file));
+			Sent current = text_head; 
+			output.write("<html>"); 
+			output.write("<head>"); 
+			output.write("<title>"); 
+			output.write("Uno Game in Html </title>"); 
+			output.write("</head>"); 
+			output.write("<body>"); 
+			while(current!=null) {
+				String table = "<p>"+current.sentence+"</p>";
+				output.write(table);
+				current = current.next;
+			}
+			output.write("</body>"); 
+			output.write("</html>"); 
+			output.close();
+		}catch (IOException e) {
+			System.out.println("Couldn't find any file.\n");
+		}
+		
+	}
+	
 	
 	public void play_cards(){
 		Node current = head; 
@@ -230,7 +317,9 @@ public class CreateDeck {
 					int num;
 					if (remainder == 3) {
 						num = findIndex(suits, current.color);
-						System.out.println("Working on the " + above[remainder]+" Card"+" color "+suits[num]);
+						//System.out.println("Working on the " + above[remainder]+" Card"+" color "+suits[num]);
+						//String opi=  "Working on the " + above[remainder]+" Card"+" color "+suits[num];
+						//pen.create_sentence(opi); 
 						exerciseCount[num] += exerciseCount[num]; 
 					}
 					if (remainder == 0){
@@ -252,15 +341,21 @@ public class CreateDeck {
 		}
 		
 		for (int i = 0; i<5; i++) {
-			if (exerciseCount[i] > 0)
-				System.out.println("For the : "+suits[i] + " card, you total is " + exerciseCount[i] +" "+punishment[i]);
-			else
-				System.out.println("No "+punishment[i] +" for this round, since you don't have the card.");
+			if (exerciseCount[i] > 0) {
+				String paper = "For the : "+suits[i] + " card, you total is " + exerciseCount[i] +" "+punishment[i];
+				pen.create_sentence(paper);
+			}
+			else {
+				String paper = "No "+punishment[i] +" for this round, since you don't have the card.";
+				pen.create_sentence(paper);
+			}
 		}
 	}
 	
+	
+	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		 /*
 		CreateDeck list = new CreateDeck(); 
@@ -289,7 +384,7 @@ public class CreateDeck {
 		String color_prec[] = {"+4","Wild","Green", "Red", "Yellow", "Blue"}; 
 		 
 		
-		System.out.println("Sorting the list.");
+		//System.out.println("Sorting the list.");
 		
 		hand.sortBubble(color_prec);
 		hand.sortRank(); 
@@ -298,6 +393,9 @@ public class CreateDeck {
 		hand.display(); 
 		
 		hand.play_cards();
+		
+		pen.display_xyz();
+		pen.display_text();
 		//Sort end
 	}
 
